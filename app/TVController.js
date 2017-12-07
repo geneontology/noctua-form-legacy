45bd7e46d2673c9220d1d1d20f19f8112950375a
tmushayahama <tmushayahama@gmail.com>
2017-12-06 17:35:32 -0800

    enh: init chips for complex annoton

--- app/TVController.js
+++ app/TVController.js
@@ -33,6 +33,99 @@ export default class TVController {
     };
     tvc.viewMode.selected = tvc.viewMode.options.table;
 
+
+    tvc.vegObjs = [{
+        'name': 'Broccoli',
+        'type': 'Brassica'
+      },
+      {
+        'name': 'Cabbage',
+        'type': 'Brassica'
+      },
+      {
+        'name': 'Carrot',
+        'type': 'Umbelliferous'
+      }
+    ];
+
+    tvc.readonly = false;
+    tvc.selectedItem = null;
+    tvc.searchText = null;
+    tvc.querySearch = querySearch;
+    tvc.vegetables = loadVegetables();
+    tvc.selectedVegetables = [];
+    tvc.numberChips = [];
+    tvc.numberChips2 = [];
+    tvc.numberBuffer = '';
+    tvc.autocompleteDemoRequireMatch = true;
+    tvc.transformChip = transformChip;
+
+    /**
+     * Return the proper object when the append is called.
+     */
+    function transformChip(chip) {
+      // If it is an object, it's already a known chip
+      if (angular.isObject(chip)) {
+        return chip;
+      }
+
+      // Otherwise, create a new one
+      return {
+        name: chip,
+        type: 'new'
+      }
+    }
+
+    /**
+     * Search for vegetables.
+     */
+    function querySearch(query) {
+      var results = query ? tvc.vegetables.filter(createFilterFor(query)) : [];
+      return results;
+    }
+
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+
+      return function filterFn(vegetable) {
+        return (vegetable._lowername.indexOf(lowercaseQuery) === 0) ||
+          (vegetable._lowertype.indexOf(lowercaseQuery) === 0);
+      };
+
+    }
+
+    function loadVegetables() {
+      var veggies = [{
+          'name': 'Broccoli',
+          'type': 'Brassica'
+        },
+        {
+          'name': 'Cabbage',
+          'type': 'Brassica'
+        },
+        {
+          'name': 'Carrot',
+          'type': 'Umbelliferous'
+        },
+        {
+          'name': 'Lettuce',
+          'type': 'Composite'
+        },
+        {
+          'name': 'Spinach',
+          'type': 'Goosefoot'
+        }
+      ];
+
+      return veggies.map(function (veg) {
+        veg._lowername = veg.name.toLowerCase();
+        veg._lowertype = veg.type.toLowerCase();
+        return veg;
+      });
+    }
     /* Init the form grid */
     //tvc.formGrid.registerApi();
     tvc.formGrid.initalizeForm();
@@ -72,7 +165,7 @@ export default class TVController {
       //let oldValue = this.editingModel[field];
       // console.log('getTerm', field, oldValue, term);
       result = this.lookup.golrLookup(field); // delete?, this.fieldToRoot[field]);
-      // console.log('result', result);
+      console.log('result', result);
     }
     return result;
   }
